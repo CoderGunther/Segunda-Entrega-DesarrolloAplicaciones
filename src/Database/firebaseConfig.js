10edaeac10959a5ece60366c490ebb28d352c906
CoderGunther <126516532+CoderGunther@users.noreply.github.com>
2023-08-31 18:53:07 -0300

    Add files via upload

---
+++ src/Database/firebaseConfig.js
@@ -0,0 +1,5 @@
+export const realtime_database_url = 'https://myecommerce-bde6e-default-rtdb.firebaseio.com/'
+
+export const api_key = 'AIzaSyCQTg0fIcODlXunRG2UrYCf6p-lD53NSv8'
+
+export const google_maps_api_key = 'AIzaSyD8oq6_0Gnfw34oU-1R4krgABk02VQNqTA'
